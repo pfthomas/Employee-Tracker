eace89779f22fd7a3ea124dd62068fed6c5ff3b1
pftho <pf.thomas97@gmail.com>
2022-09-28 23:47:33 -0400

    issues with server connection

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+USE employee_db;
+
+INSERT INTO department (dpt_name)
+VALUES 
+('Sales'), 
+('Engineering'), 
+('Finance'), 
+('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+("John", "Dart", 1),
+("Mike", "Chan", 2),
+("Ashley", "Rodriguez", 3),
+("Kevin", "Tupik", 4),
+("Kunal", "Singh", 5),
+("Malia", "Brown", 6),
+("Sarah", "Lourd", 7),
+("Tom", "Allen", 8);
+
